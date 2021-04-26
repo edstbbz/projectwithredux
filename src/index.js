20a70864879628ae084150d0885a799990a7c605
edstbbz <ed_stubbz@mail.ru>
2021-04-26 19:34:57 +0400

    init repos

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
-import {rootReducer} from './store/reducers/rootReducer'
+import { rootReducer } from './store/reducers/rootReducer';
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={ store }>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
