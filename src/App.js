0c89441abe80fa4329c2fc26c32f0974565fb80b
edstbbz <ed_stubbz@mail.ru>
2021-04-26 22:35:50 +0400

    small fix app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { useSelector } from 'react-redux';
 import styles from './App.module.css';
+import { useSelector } from 'react-redux';
 import { LoginPage } from './pages/loginPage/loginPage';
 
 const App = () => {
-  const User = useSelector((state) => state.user.name);
-  return <div className={styles.App}>{!User && <LoginPage />}</div>;
+  const currentUser = useSelector((state) => state.user.name);
+  if(!currentUser) {
+    return <LoginPage />
+  }
+  return <div className={styles.App}>Hello, {currentUser}! </div>;
 }
 
 export default App;
